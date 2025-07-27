ecbf646c9c45fb3018e510b6b1e32cb0429979fb
오다혜 <devohda123@gmail.com>
2025-07-27 11:47:18 +0900

    feat: 각 에셋에 radian 추가

--- src/services/assets.ts
+++ src/services/assets.ts
@@ -9,19 +9,21 @@ interface makeBoxesProps {
 
 export const makeBoxes = ({ world, count, container }: makeBoxesProps) => {
   const boxes = new Array(count).fill(null).map(() => {
-    const width = getRandomInt(500, 600);
-    const height = getRandomInt(500, 600);
+    const width = getRandomInt(300, 600);
+    const height = getRandomInt(300, 600);
 
     const x = getRandomInt(800, container.offsetWidth - 800);
     const y = getRandomInt(-800, -500);
 
-    const friction = getRandFloat(0.005, 0.02);
-    const restitution = getRandFloat(0.7, 1);
+    const friction = getRandFloat(0.05, 0.1);
+    const restitution = getRandFloat(0.7, 0.8);
+    const angle = getRandFloat((0 * Math.PI) / 180, (90 * Math.PI) / 180);
 
     return Bodies.rectangle(x, y, width, height, {
       render: { visible: true },
       friction,
       restitution,
+      angle,
     });
   });
 
